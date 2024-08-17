ea6bf1192b083da23a7b4e13fe6d3ea3244c34d0
Chris Mamon <chrisam1993@gmail.com>
2024-08-18 00:42:03 +1000

    feature: added guards to check that the appropriate assets are installed

--- my-profile/fzf.sh
+++ my-profile/fzf.sh
@@ -1,15 +1,26 @@
-[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+#shellcheck disable=SC1091
+FZF_BASH="$HOME/.fzf.bash"
 
-export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
-export FZF_CTRL_T_COMAND="$FZF_DEFAULT_COMMAND"
-export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
+if [[ -f $FZF_BASH ]]; then
+    #shellcheck disable=SC1090
+    [ -f "$FZF_BASH" ] && source "$FZF_BASH"
 
-_fzf_compgen_path() {
-    fd --hidden --exclude .git . "$1"
-}
+    if type fd &>/dev/null; then
+        export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
+        export FZF_CTRL_T_COMAND="$FZF_DEFAULT_COMMAND"
+        export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
 
-_fzf_compgen_dir() {
-    fd --type=d --hidden --exclude .git . "$1"
-}
+        _fzf_compgen_path() {
+            fd --hidden --exclude .git . "$1"
+        }
 
-source $PROFILE_FOLDER/fzf-git.sh
+        _fzf_compgen_dir() {
+            fd --type=d --hidden --exclude .git . "$1"
+        }
+
+    else
+        echoinfo "fd not installed, shortcuts excluded"
+    fi
+
+    source "$PROFILE_FOLDER/fzf-git.sh"
+fi
