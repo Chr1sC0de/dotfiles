90b2b398023c4a650bbc147540420311d56bef14
Chris Mamon <chrisam1993@gmail.com>
2024-08-18 16:20:35 +1000

    feature: refactored

--- utils/install-dotfiles.sh
+++ utils/install-dotfiles.sh
@@ -6,15 +6,17 @@ SCRIPT_DIR="$(dirname -- "${BASH_SOURCE[0]}")"
 source "$SCRIPT_DIR/core.sh"
 
 for DOTNAME in "${DOTNAMES[@]}"; do
-    echoinfo "\e[36mCreating symlink for $DOTNAME"
+    # echoinfo "\e[36mCreating symlink for $DOTNAME"
+    #
+    # SYMLINK_TARGET="$HOME/.$DOTNAME"
+    #
+    # if [[ ! -L $SYMLINK_TARGET ]]; then
+    #     echoinfo "\e[31m$DOTNAME symlink not found, creating"
+    #     ln -s "$DOTFILE_DIR/$DOTNAME" "$SYMLINK_TARGET"
+    #     echoinfo "\e[31mFinished creating $DOTNAME symlink"
+    # else
+    #     echoinfo "\e[31m$DOTNAME symlink already exists"
+    # fi
+    "$SCRIPT_DIR"/"create-symlink.sh" "$DOTNAME"
 
-    SYMLINK_TARGET="$HOME/.$DOTNAME"
-
-    if [[ ! -L $SYMLINK_TARGET ]]; then
-        echoinfo "\e[31m$DOTNAME symlink not found, creating"
-        ln -s "$DOTFILE_DIR/$DOTNAME" "$SYMLINK_TARGET"
-        echoinfo "\e[31mFinished creating $DOTNAME symlink"
-    else
-        echoinfo "\e[31m$DOTNAME symlink already exists"
-    fi
 done
