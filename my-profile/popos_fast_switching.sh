3427fefa7f32b67a56efa40bba5a2a30964d2a33
Chris Mamon <chrisam1993@gmail.com>
2024-09-07 08:05:23 +1000

    feature: updated nvim and added a check to enable popos_fast_switching

--- my-profile/popos_fast_switching.sh
+++ my-profile/popos_fast_switching.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-gsettings set org.gnome.mutter dynamic-workspaces false
-gsettings set org.gnome.desktop.wm.preferences num-workspaces 9
-for i in {1..9}; do
-    gsettings set "org.gnome.shell.keybindings" "switch-to-application-$i" "[]"
-    gsettings set "org.gnome.desktop.wm.keybindings" "switch-to-workspace-$i" "['<Super>${i}']"
-    gsettings set "org.gnome.desktop.wm.keybindings" "move-to-workspace-$i" "['<Super><Shift>${i}']"
-done
+if [[ $(cmd <"/etc/os-release" | grep ^NAME= | cut -d= -f2) = "Pop!_OS" ]]; then
+    gsettings set org.gnome.mutter dynamic-workspaces false
+    gsettings set org.gnome.desktop.wm.preferences num-workspaces 9
+    for i in {1..9}; do
+        gsettings set "org.gnome.shell.keybindings" "switch-to-application-$i" "[]"
+        gsettings set "org.gnome.desktop.wm.keybindings" "switch-to-workspace-$i" "['<Super>${i}']"
+        gsettings set "org.gnome.desktop.wm.keybindings" "move-to-workspace-$i" "['<Super><Shift>${i}']"
+    done
+fi
