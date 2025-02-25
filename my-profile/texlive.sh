f8dc32ed7a68f3aa0a6787ac15cd27ffca631c2f
Chris Mamon <chrisam1993@gmail.com>
2025-02-25 22:43:46 +1100

    update

---
+++ my-profile/texlive.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+TEXLIVE_DIR="/usr/local/texlive/2024/bin/x86_64-linux"
+
+if [[ -d $TEXLIVE_DIR ]]; then
+    if [[ $PATH != *"$TEXLIVE_DIR"* ]]; then
+        export PATH="$PATH:$TEXLIVE_DIR"
+    fi
+fi
