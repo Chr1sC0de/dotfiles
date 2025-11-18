40a0e8fe912e50e28b5ddeb2c2043aa6ad626a2d
Chris Mamon <chrisam1993@gmail.com>
2025-11-19 00:11:40 +1100

    update

---
+++ utils/dotfiles-install.sh
@@ -0,0 +1,51 @@
+SCRIPT_DIR="$(dirname -- "${BASH_SOURCE[0]}")"
+
+source "$SCRIPT_DIR/common.sh"
+
+TO_INSTALL=("${DEFAULTS[@]}")
+KWARGS=()
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+    -h | --help)
+        echo "Usage: install-dotfiles.sh [OPTIONS]"
+        echo
+        echo "Options:"
+        echo "  -h, --help            Show this help"
+        echo "  -a, --all             Include all extras"
+        echo "  -k, --kitty           Include kitty config"
+        echo "  -t, --tmux            Include tmux config"
+        echo "  -g, --gitconfig       Include gitconfig"
+        echo "  -x, --xdg-terminals   Include xdg-terminals config"
+        echo "  -v, --verbose         show logs"
+        echo "  -f, --force           force install symlink"
+        exit 0
+        ;;
+    -k | --kitty)
+        TO_INSTALL+=("kitty" "config/kitty")
+        ;;
+    -t | --tmux)
+        TO_INSTALL+=("tmux.conf")
+        ;;
+    -g | --gitconfig)
+        TO_INSTALL+=("gitconfig")
+        ;;
+    -x | --xdg-terminals)
+        TO_INSTALL+=("config/xdg-terminals.list")
+        ;;
+    -v | --verbose)
+        KWARGS+=("-v")
+        ;;
+    -f | --force)
+        KWARGS+=("-f")
+        ;;
+    -a | --all)
+        TO_INSTALL+=("${EXTRAS[@]}")
+        ;;
+    esac
+    shift
+done
+
+for INSTALL in "${TO_INSTALL[@]}"; do
+    "$SCRIPT_DIR"/"symlink-create.sh" "$INSTALL" "${KWARGS[@]}"
+done
---
+++ utils/symlink-create.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(dirname -- "${BASH_SOURCE[0]}")"
+
+source "$SCRIPT_DIR/common.sh"
+
+FORCE=false
+export VERBOSE=false
+
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+    -h | --help)
+        echo "Usage: create-symlink.sh [OPTIONS] [TARGET]"
+        echo "create symbolic links for the following valid targets"
+        for LINK in "${DOTNAMES[@]}"; do
+            echo "  $LINK"
+        done
+        echo "Options:"
+        echo "  -h, --help        display this message"
+        echo "  -f, --force       forcibly remove symlink if exists"
+        echo "  -v, --verbose     show logs"
+        exit 0
+        ;;
+    -f | --force)
+        FORCE=true
+        ;;
+    -v | --verbose)
+        VERBOSE=true
+        ;;
+    *)
+        if [[ -z $TARGET ]]; then
+            TARGET=$1
+        fi
+        ;;
+    esac
+    shift
+done
+
+set -e
+
+if [[ -z $TARGET ]]; then
+    echoinfo "No input provided, skipping"
+    exit 1
+fi
+
+COLORDOTNAME="\e[36m$TARGET\e[0m"
+
+if [[ ! ${DOTNAMES[*]} =~ $TARGET ]]; then
+    echoinfo "$COLORDOTNAME not in available dotfiles\e[36m"
+    echo "["
+    for NAME in "${DOTNAMES[@]}"; do
+        echo "  $NAME"
+    done
+    echo "]"
+    echoinfo "Input dotfile name should not start with ."
+    exit 1
+fi
+
+echoinfo "Creating symlink for $COLORDOTNAME"
+
+SYMLINK_TARGET="$HOME/.$TARGET"
+
+if [[ ! -L $SYMLINK_TARGET ]]; then
+    echoinfo "$COLORDOTNAME symlink not found, creating"
+    ln -s -v "$DOTFILE_DIR/$TARGET" "$SYMLINK_TARGET"
+    echoinfo "Finished creating $COLORDOTNAME symlink"
+elif $FORCE; then
+    echoinfo "Forcefully creating symlink"
+    rm -f "$SYMLINK_TARGET"
+    ln -s -v "$DOTFILE_DIR/$TARGET" "$SYMLINK_TARGET"
+    echoinfo "Finished creating $COLORDOTNAME symlink"
+else
+    echoinfo "$COLORDOTNAME symlink already exists, skipping"
+fi
