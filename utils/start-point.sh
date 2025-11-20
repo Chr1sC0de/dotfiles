f589ed800f76ff05fe2fed6d876da978f6c44302
Chris Mamon <chrisam1993@gmail.com>
2025-11-21 08:55:31 +1100

    update

--- utils/start-point.sh
+++ utils/start-point.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-sudo apt install git openssh-server
+sudo apt install git openssh-server -y
 
 mkdir -p GitHub
 
 cd GitHub || exit
 
-git glone https://github.com/Chr1sC0de/dotfiles.git
+git clone https://github.com/Chr1sC0de/dotfiles.git
