4ca08605223ea7924397ebd5de3b25a6fb3fcbd4
Chris Mamon <chrisam1993@gmail.com>
2025-11-21 21:05:12 +1100

    update

--- start-point.sh
+++ start-point.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 
 sudo apt install git -y
-
 cd "$HOME" || exit
-
 mkdir -p GitHub
-
 cd GitHub || exit
-
 git clone https://github.com/Chr1sC0de/dotfiles.git
-
 cd dotfiles || exit
