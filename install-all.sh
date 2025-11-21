19e206f01517f753b560aa5fc533e94b75999fb1
Chris Mamon <chrisam1993@gmail.com>
2025-11-21 20:40:49 +1100

    update

--- install-all.sh
+++ install-all.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR="$(dirname -- "${BASH_SOURCE[0]}")"
 UTILS_DIR="$SCRIPT_DIR/utils"
 
 bash "$UTILS_DIR/dependencies-install-all.sh"
-bash "$UTILS_DIR/dotfiles-install.sh" --tmux --gitconfig
+bash "$UTILS_DIR/dotfiles-install.sh" -f
 bash "$UTILS_DIR/profile-to-bashrc.sh"
 
 . "$HOME/.bashrc"
--- utils/dependencies-install-all.sh
+++ utils/dependencies-install-all.sh
@@ -4,16 +4,46 @@ SCRIPT_DIR="$(dirname -- "${BASH_SOURCE[0]}")"
 
 . "$SCRIPT_DIR/common.sh"
 
-SCRIPT_FILENAMES=(
+CORE_SCRIPTS=(
     'core-dependencies-install.sh'
-    'gh-install.sh'
-    'ghostty-install.sh'
-    'neovim-install.sh'
-    'nvm-install.sh'
     'rust-install.sh'
+    'nvm-install.sh'
     'uv-install.sh'
+    'neovim-install.sh'
 )
 
-for SCRIPT_FILENAME in "${SCRIPT_FILENAMES[@]}"; do
+EXTRA_SCRIPTS=(
+    'gh-install.sh'
+    'ghostty-install.sh'
+)
+
+TO_INSTALL=("${CORE_SCRIPTS[@]}")
+
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+    -h | --help)
+        echo "Usage: dependencies-install-all.sh [OPTIONS]"
+        echo
+        echo "Options:"
+        echo "  -h, --help            Show this help"
+        echo "  -a, --all             Include all extras"
+        echo "  -g, --github          Include github"
+        echo "  -G, --ghostty         Include ghostty"
+        exit 0
+        ;;
+    -a | --all)
+        TO_INSTALL+=("${EXTRA_SCRIPTS[@]}")
+        ;;
+    -g | --github)
+        TO_INSTALL+=("gh-install.sh")
+        ;;
+    -G | --ghostty)
+        TO_INSTALL+=("ghostty-install.sh")
+        ;;
+    esac
+    shift
+done
+
+for SCRIPT_FILENAME in "${TO_INSTALL[@]}"; do
     bash "$DEPENDENCIES_SCRIPTS_FOLDER/$SCRIPT_FILENAME"
 done
