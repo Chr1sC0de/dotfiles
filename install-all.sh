e25834d113e5cd66fc6f1e63f9182b8006443e61
Chris Mamon <chrisam1993@gmail.com>
2025-11-21 19:30:08 +1100

    update

--- install-all.sh
+++ install-all.sh
@@ -1,9 +1,8 @@
-#!/usr/bin/end bash
+#!/usr/bin/env bash
 
 SCRIPT_DIR="$(dirname -- "${BASH_SOURCE[0]}")"
 
 UTILS_DIR="$SCRIPT_DIR/utils"
 
 bash "$UTILS_DIR/dependencies-install-all.sh"
-
 bash "$UTILS_DIR/dotfiles-install.sh" --tmux --gitconfig
