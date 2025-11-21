448970cda75ea4abe88462f94552fc396b235a87
Chris Mamon <chrisam1993@gmail.com>
2025-11-21 19:26:27 +1100

    update

--- install-all.sh
+++ install-all.sh
@@ -1,1 +1,9 @@
 #!/usr/bin/end bash
+
+SCRIPT_DIR="$(dirname -- "${BASH_SOURCE[0]}")"
+
+UTILS_DIR="$SCRIPT_DIR/utils"
+
+bash "$UTILS_DIR/dependencies-install-all.sh"
+
+bash "$UTILS_DIR/dotfiles-install.sh"
