fd0ddea0fc8909e73e0b03eb1843804a78294106
Chris Mamon <chrisam1993@gmail.com>
2025-11-21 19:33:34 +1100

    update

--- utils/dependencies-scripts/gh-install.sh
+++ utils/dependencies-scripts/gh-install.sh
@@ -16,4 +16,6 @@ sudo mkdir -p -m 755 /etc/apt/sources.list.d
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null
 
-sudo apt update sudo apt install gh -y
+sudo apt update
+
+sudo apt install gh -y
