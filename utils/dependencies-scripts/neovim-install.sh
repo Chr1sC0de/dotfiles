3e99197f4f3964cb4078ad4e90302993f4fe7b95
Chris Mamon <chrisam1993@gmail.com>
2025-12-04 13:34:17 +0000

    update

--- utils/dependencies-scripts/neovim-install.sh
+++ utils/dependencies-scripts/neovim-install.sh
@@ -6,4 +6,5 @@
     curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
     tar -xzf nvim-linux-x86_64.tar.gz
     cp -afr nvim-linux-x86_64/* "$HOME/.local"
+    rm -rf "$HOME"/Downloads/
 )
