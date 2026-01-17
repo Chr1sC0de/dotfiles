bb9271be2323076f52c1f4a87d4d3f43e003e336
Chris Mamon <chrisam1993@gmail.com>
2026-01-17 14:31:19 +0000

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
-#!/usr/bin/env bash
+#!/usr/bin/env -S bash -x
+
+sudo apt-get install git -y
+cd "$HOME" || exit 1
+mkdir -p GitHub
+cd GitHub || exit 1
+git clone https://github.com/Chr1sC0de/dotfiles.git
+cd dotfiles || exit 1
 
 export DEBIAN_FRONTEND=noninteractive
 
