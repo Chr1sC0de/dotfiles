bc171b8a07a31710745424ca7d027c977132de95
Chris Mamon <chrisam1993@gmail.com>
2026-01-18 14:15:19 +1100

    update

--- utils/dependencies-install-all.sh
+++ utils/dependencies-install-all.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env -S bash -x
 
 SCRIPT_DIR="$(dirname -- "${BASH_SOURCE[0]}")"
 
--- utils/profile-to-bashrc.sh
+++ utils/profile-to-bashrc.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env -S bash -x
 grep "$HOME/.my-profile/__init__.sh" "$HOME"/.bashrc
 
 if [[ ! $(grep "$HOME/.my-profile/__init__.sh" "$HOME"/.bashrc) ]]; then
