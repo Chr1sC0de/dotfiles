f556c1b74a76511f0bf3dfba2361665163a28e48
Chris Mamon <chrisam1993@gmail.com>
2026-01-26 01:52:49 +1100

    update

--- utils/dependencies-scripts/uv-install.sh
+++ utils/dependencies-scripts/uv-install.sh
@@ -4,6 +4,6 @@ curl -LsSf https://astral.sh/uv/install.sh | bash
 
 # since uv is now installed, install all the othe scripts
 #
-"$HOME"/.local/bin/uv tool install pre-commit
+"$HOME"/.local/bin/uv tool install prek
 "$HOME"/.local/bin/uv tool install toml2json
 "$HOME"/.local/bin/uv tool install neovim-remote
