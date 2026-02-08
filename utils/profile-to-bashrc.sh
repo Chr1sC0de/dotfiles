6c97dd6a593cc24431af57928ca31b5440bfee62
Chris Mamon <chrisam1993@gmail.com>
2026-02-08 20:07:28 +1100

    update

--- utils/profile-to-bashrc.sh
+++ utils/profile-to-bashrc.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-grep "$HOME/.my-profile/__init__.sh" "$HOME"/.bashrc
 
-if [[ ! $(grep "$HOME/.my-profile/__init__.sh" "$HOME"/.bashrc) ]]; then
+if [[ ! $(grep '$HOME/.my-profile/__init__.sh' "$HOME"/.bashrc) ]]; then
     echo '. "$HOME/.my-profile/__init__.sh"' >>"$HOME"/.bashrc
 fi
