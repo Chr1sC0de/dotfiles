9364a5040ac31a5c6839886201ae55f26f676b89
Chris Mamon <chrisam1993@gmail.com>
2026-02-12 01:47:31 +1100

    update

--- my-profile/fzf.sh
+++ my-profile/fzf.sh
@@ -2,28 +2,29 @@
 
 # source fzf
 if [[ ! "$PATH" == *"$HOME/.fzf/bin"* ]]; then
-    PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
-    eval "$(fzf --bash)"
+	PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
 
-if [[ "$(type fzf &>/dev/null)" ]]; then
+if type fzf &>/dev/null; then
 
-    if type fd &>/dev/null; then
-        export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
-        export FZF_CTRL_T_COMAND="$FZF_DEFAULT_COMMAND"
-        export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
+	if type fd &>/dev/null; then
+		export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
+		export FZF_CTRL_T_COMAND="$FZF_DEFAULT_COMMAND"
+		export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
 
-        _fzf_compgen_path() {
-            fd --hidden --exclude .git . "$1"
-        }
+		_fzf_compgen_path() {
+			fd --hidden --exclude .git . "$1"
+		}
 
-        _fzf_compgen_dir() {
-            fd --type=d --hidden --exclude .git . "$1"
-        }
+		_fzf_compgen_dir() {
+			fd --type=d --hidden --exclude .git . "$1"
+		}
 
-    else
-        echo "fd not installed, shortcuts excluded"
-    fi
+	else
+		echo "fd not installed, shortcuts excluded"
+	fi
 
-    . "$PROFILE_FOLDER/fzf-git.sh"
+	. "$PROFILE_FOLDER/fzf-git.sh"
+
+	eval "$(fzf --bash)"
 fi
