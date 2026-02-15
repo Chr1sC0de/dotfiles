b3e35b104b0a0fa011843e3faea746b2cf1e5e93
Chris Mamon <chrisam1993@gmail.com>
2026-02-15 21:11:57 +1100

    update

--- utils/dependencies-scripts/lazygit-install.sh
+++ utils/dependencies-scripts/lazygit-install.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-tag=v0.58.1 && echo "tag=$tag"
-owner=jesseduffield && echo "owner=$owner"
-repo=lazygit && echo "repo=$repo"
-binary_name=lazygit && echo "binary_name=$binary_name"
-download_file=lazygit_0.58.1_linux_x86_64.tar.gz && echo "download_file=$download_file"
-release="https://github.com/$owner/$repo/releases/download/$tag/$download_file" && echo "release=$release"
+tag=v0.58.1
+owner=jesseduffield
+repo=lazygit
+binary_name=lazygit
+download_file=lazygit_0.58.1_linux_x86_64.tar.gz
+release="https://github.com/$owner/$repo/releases/download/$tag/$download_file"
 
-download_folder=/tmp && echo "download_folder=$download_folder"
-downloaded_file=$download_folder/$download_file && echo downloaded_file=$downloaded_file
+download_folder=/tmp
+downloaded_file=$download_folder/$download_file
 
-extract_folder=$download_folder/$repo && echo extract_folder=$extract_folder
-extracted_binary=$extract_folder/$binary_name && echo extracted_binary=$extracted_binary
+extract_folder=$download_folder/$repo
+extracted_binary=$extract_folder/$binary_name
 
-target_folder=$HOME/.local/bin && echo target_folder="$target_folder"
-target_file=$target_folder/$binary_name && echo target_file="$target_file"
+target_folder=$HOME/.local/bin
+target_file=$target_folder/$binary_name
 
 echo setting up $repo release $release to /tmp
 wget -P /tmp $release
