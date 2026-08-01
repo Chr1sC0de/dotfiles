e320ea45906a01527d40c6fbacd646d7d4e9dfe9
Chris Mamon <chrisam1993@gmail.com>
2026-08-02 00:53:06 +1000

    update

--- config/nvim/queries/python/injections.scm
+++ config/nvim/queries/python/injections.scm
@@ -26,35 +26,16 @@
   (#match? @script ".*_sql_*.*")
   (#set! injection.language "sql"))
 
-; jinja
-(assignment
-  left: (identifier) @script
-  right: (string
-    (string_content) @injection.content)
-  (#match? @script ".+_jinja_*.*")
-  (#set! injection.language "jinja"))
-
-; python
-(string
-  (string_content) @injection.content
-  (#match? @injection.content "#!/.*python")
-  (#set! injection.language "python"))
-
-(assignment
-  left: (identifier) @script
-  right: (string
-    (string_content) @injection.content)
-  (#match? @script ".+_python_*.*")
-  (#set! injection.language "python"))
-
 (call
-  function: (attribute
-    object: (identifier)
-    attribute: (identifier) @method)
+  function: [
+    (identifier) @method
+    (attribute
+      attribute: (identifier) @method)
+  ]
   arguments: (argument_list
     (string
       (string_start)
       (string_content) @injection.content
       (string_end)))
-  (#match? @method "sql")
+  (#match? @method ".*sql.*")
   (#set! injection.language "sql"))
