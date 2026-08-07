8f229fe69df25919a3223258ef605f18aff987c9
Chris Mamon <chrisam1993@gmail.com>
2026-08-08 01:40:57 +1000

    update

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -69,5 +69,12 @@ end
 -- command to run file
 vim.api.nvim_create_user_command("RunFile", function()
 	local file = vim.fn.expand("%:p")
-	vim.cmd("botright split | terminal " .. vim.fn.shellescape(file))
+
+	vim.cmd("botright split")
+	vim.cmd("terminal " .. vim.fn.shellescape(file))
+
+	local buf = vim.api.nvim_get_current_buf()
+	vim.bo[buf].bufhidden = "wipe"
+
+	vim.cmd("startinsert")
 end, {})
